dcf2d4ab426b4c74153d58b0c649f6da6a0747bf
yushynotfound <ege887.business@gmail.com>
2024-10-14 16:54:40 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+/*
+
+const dersVerileri = [
+    // Pazartesi
+    { gun: 'pazartesi', saat: 1, dersAdi: 'Adabımuaşeret', odev: 'Hayır', not: '' },
+    { gun: 'pazartesi', saat: 2, dersAdi: 'Temel Dini Bilgiler', odev: 'Hayır', not: '' },
+    { gun: 'pazartesi', saat: 3, dersAdi: 'Biyoloji', odev: 'Hayır', not: '' },
+    { gun: 'pazartesi', saat: 4, dersAdi: 'Biyoloji', odev: 'Hayır', not: '' },
+    { gun: 'pazartesi', saat: 5, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'pazartesi', saat: 6, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
+    { gun: 'pazartesi', saat: 7, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
+    { gun: 'pazartesi', saat: 8, dersAdi: 'Müzik', odev: 'Hayır', not: '' },
+    
+    // Salı
+    { gun: 'salı', saat: 1, dersAdi: 'Matematik', odev: 'Hayır', not: ''  },
+    { gun: 'salı', saat: 2, dersAdi: 'Matematik', odev: 'Hayır', not: ''  },
+    { gun: 'salı', saat: 3, dersAdi: 'Rehberlik', odev: 'Hayır', not: '' },
+    { gun: 'salı', saat: 4, dersAdi: 'Almanca', odev: 'Hayır', not: '' },
+    { gun: 'salı', saat: 5, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
+    { gun: 'salı', saat: 6, dersAdi: 'Sağlık', odev: 'Hayır', not: ''  },
+    { gun: 'salı', saat: 7, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'salı', saat: 8, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    
+    // Çarşamba
+    { gun: 'çarşamba', saat: 1, dersAdi: 'Fizik', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 2, dersAdi: 'Fizik', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 3, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 4, dersAdi: 'Coğrafya', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 5, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 6, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 7, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 8, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
+    
+    // Perşembe
+    { gun: 'perşembe', saat: 1, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 1, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 3, dersAdi: 'Almanca', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 4, dersAdi: 'Coğrafya', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 5, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 6, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 7, dersAdi: 'Din', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 8, dersAdi: 'Tarih', odev: 'Hayır', not: '' },
+    
+    // Cuma
+    { gun: 'cuma', saat: 1, dersAdi: 'Müzik', odev: 'Hayır', not: '' },
+    { gun: 'cuma', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'cuma', saat: 3, dersAdi: 'Din Kültürü ve Ahlak Bilgisi', odev: 'Hayır', not: '' },
+    { gun: 'cuma', saat: 4, dersAdi: 'Tarih', odev: 'Hayır', not: '' },
+    { gun: 'cuma', saat: 5, dersAdi: 'Kimya', odev: 'Hayır', not: '' },
+    { gun: 'cuma', saat: 6, dersAdi: 'Kimya', odev: 'Hayır', not: '' },
+    { gun: 'cuma', saat: 7, dersAdi: 'Beden Eğitimi', odev: 'Hayır', not: 'Top getirin' },
+    { gun: 'cuma', saat: 8, dersAdi: 'Beden Eğitimi', odev: 'Hayır', not: 'Top getirin' },
+];
+
+*/
+
 // Ders Verilerini Tanımlama
 const dersVerileri = [
     // Pazartesi
@@ -14,41 +72,41 @@ const dersVerileri = [
     { gun: 'salı', saat: 1, dersAdi: 'Matematik', odev: 'Evet', not: 'Hocanın attığı testler'  },
     { gun: 'salı', saat: 2, dersAdi: 'Matematik', odev: 'Evet', not: 'Hocanın attığı testler'  },
     { gun: 'salı', saat: 3, dersAdi: 'Rehberlik', odev: 'Hayır', not: '' },
-    { gun: 'salı', saat: 4, dersAdi: 'Almanca', odev: 'Hayır', not: '' },
-    { gun: 'salı', saat: 5, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
+    { gun: 'salı', saat: 4, dersAdi: 'Almanca', odev: 'Evet', not: 'Kitap ödevi' },
+    { gun: 'salı', saat: 5, dersAdi: 'İngilizce', odev: 'Evet', not: 'Workbook' },
     { gun: 'salı', saat: 6, dersAdi: 'Sağlık', odev: 'Hayır', not: ''  },
-    { gun: 'salı', saat: 7, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
-    { gun: 'salı', saat: 8, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'salı', saat: 7, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Evet', not: "San'at şiiri" },
+    { gun: 'salı', saat: 8, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Evet', not: "San'at şiiri" },
     
     // Çarşamba
     { gun: 'çarşamba', saat: 1, dersAdi: 'Fizik', odev: 'Hayır', not: '' },
     { gun: 'çarşamba', saat: 2, dersAdi: 'Fizik', odev: 'Hayır', not: '' },
     { gun: 'çarşamba', saat: 3, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
     { gun: 'çarşamba', saat: 4, dersAdi: 'Coğrafya', odev: 'Hayır', not: '' },
-    { gun: 'çarşamba', saat: 5, dersAdi: 'Matematik', odev: 'Evet', not: 'üslü sayı + köklü sayı testi' },
-    { gun: 'çarşamba', saat: 6, dersAdi: 'Matematik', odev: 'Evet', not: 'üslü sayı + köklü sayı testi' },
+    { gun: 'çarşamba', saat: 5, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
+    { gun: 'çarşamba', saat: 6, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
     { gun: 'çarşamba', saat: 7, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
     { gun: 'çarşamba', saat: 8, dersAdi: 'İngilizce', odev: 'Hayır', not: '' },
     
     // Perşembe
-    { gun: 'perşembe', saat: 1, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Evet', not: 'Kitap etkinliği' },
-    { gun: 'perşembe', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Evet', not: 'Kitap etkinliği' },
     { gun: 'perşembe', saat: 1, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
-    { gun: 'perşembe', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Evet', not: 'Şiir çalışmaları' },
+    { gun: 'perşembe', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 1, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
+    { gun: 'perşembe', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
     { gun: 'perşembe', saat: 3, dersAdi: 'Almanca', odev: 'Hayır', not: '' },
     { gun: 'perşembe', saat: 4, dersAdi: 'Coğrafya', odev: 'Hayır', not: '' },
     { gun: 'perşembe', saat: 5, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
     { gun: 'perşembe', saat: 6, dersAdi: 'Matematik', odev: 'Hayır', not: '' },
     { gun: 'perşembe', saat: 7, dersAdi: 'Din', odev: 'Hayır', not: '' },
-    { gun: 'perşembe', saat: 8, dersAdi: 'Tarih', odev: 'Evet', not: '21-30' },
+    { gun: 'perşembe', saat: 8, dersAdi: 'Tarih', odev: 'Evet', not: 'Kitap etkinlikleri ve özet çıkarma' },
     
     // Cuma
-    { gun: 'cuma', saat: 1, dersAdi: 'Müzik', odev: 'Evet', not: 'İstiklal Marşı' },
+    { gun: 'cuma', saat: 1, dersAdi: 'Müzik', odev: 'Hayır', not: '' },
     { gun: 'cuma', saat: 2, dersAdi: 'Türk Dili ve Edebiyatı', odev: 'Hayır', not: '' },
     { gun: 'cuma', saat: 3, dersAdi: 'Din Kültürü ve Ahlak Bilgisi', odev: 'Hayır', not: '' },
     { gun: 'cuma', saat: 4, dersAdi: 'Tarih', odev: 'Hayır', not: '' },
-    { gun: 'cuma', saat: 5, dersAdi: 'Kimya', odev: 'Evet', not: 'Slayt' },
-    { gun: 'cuma', saat: 6, dersAdi: 'Kimya', odev: 'Evet', not: 'Slayt' },
+    { gun: 'cuma', saat: 5, dersAdi: 'Kimya', odev: '', not: '' },
+    { gun: 'cuma', saat: 6, dersAdi: 'Kimya', odev: '', not: '' },
     { gun: 'cuma', saat: 7, dersAdi: 'Beden Eğitimi', odev: 'Hayır', not: 'Top getirin' },
     { gun: 'cuma', saat: 8, dersAdi: 'Beden Eğitimi', odev: 'Hayır', not: 'Top getirin' },
 ];
